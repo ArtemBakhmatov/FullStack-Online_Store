5b77c64f792297a8db7089f20556cdeb59e21bfd
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-07-23 22:24:34 +0300

    FRONTEND - Главная страница, список товаров, типов, брендов

---
+++ client/src/components/DeviceItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+
+import star from '../assets/star.svg';
+import { DEVICE_ROUTE } from '../utils/consts';
+
+const DeviceItem = ({ device }) => {
+    const navigate = useNavigate();
+
+    return (
+        <Col md={3} className='mt-3' onClick={ () => navigate(DEVICE_ROUTE + '/' + device.id) }>
+            <Card style={{ width: 150, cursor: 'pointer'}} border='light'>
+                <Image width={150} height={150} src={ device.img } />
+                <div className='text-black-50 mt-1 d-flex justify-content-between align-items-center'>
+                    <div>Samsung...</div>
+                    <div className='d-flex align-items-center'>
+                        <div>{ device.rating }</div>
+                        <Image src={ star } style={{ height: 18, width: 18, marginLeft: 5}} />
+                    </div>
+                </div>
+                <div>{ device.name }</div> 
+            </Card>
+        </Col>
+    );
+};
+
+export default DeviceItem;
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -4,18 +4,26 @@ export default class DeviceStore {
     constructor() {
         this._types = [
             { id: 1, name: 'Холодильники' },
-            { id: 2, name: 'Смартфоны' }
+            { id: 2, name: 'Смартфоны' },
+            { id: 3, name: 'Ноутбуки' },
+            { id: 4, name: 'Телевизоры' }
         ];
         this._brands = [
             { id: 1, name: 'Samsung' },
-            { id: 2, name: 'Apple' }
+            { id: 2, name: 'Apple' },
+            { id: 3, name: 'Lenovo' },
+            { id: 4, name: 'Asus' },
+            
+
         ];
         this._devices = [
             { id: 1, name: 'Iphone 12 pro', price: 25000, rating: 5, img: 'https://tehno37.ru/image/cache/catalog/alexeioblojki/%21%21%21%21apple/12promax/iphone12promax512blue1-400x300.jpg'},
             { id: 2, name: 'Iphone 12 pro', price: 25000, rating: 5, img: 'https://tehno37.ru/image/cache/catalog/alexeioblojki/%21%21%21%21apple/12promax/iphone12promax512blue1-400x300.jpg'},
             { id: 3, name: 'Iphone 12 pro', price: 25000, rating: 5, img: 'https://tehno37.ru/image/cache/catalog/alexeioblojki/%21%21%21%21apple/12promax/iphone12promax512blue1-400x300.jpg'},
-            { id: 4, name: 'Iphone 12 pro', price: 25000, rating: 5, img: 'https://tehno37.ru/image/cache/catalog/alexeioblojki/%21%21%21%21apple/12promax/iphone12promax512blue1-400x300.jpg'}   
-        ]
+            { id: 4, name: 'Iphone 12 pro', price: 25000, rating: 5, img: 'https://tehno37.ru/image/cache/catalog/alexeioblojki/%21%21%21%21apple/12promax/iphone12promax512blue1-400x300.jpg'}
+        ];
+        this._selectedType = {};
+        this._selectedBrand = {};
         makeAutoObservable(this);
     }
 
@@ -31,6 +39,14 @@ export default class DeviceStore {
         this._devices = devices;
     }
 
+    setSelectedType(type) {
+        this._selectedType = type;
+    }
+
+    setSelectedBrand(brand) {
+        this._selectedBrand = brand;
+    }
+
     get types() {
         return this._types;
     }
@@ -41,5 +57,13 @@ export default class DeviceStore {
     
     get devices() {
         return this._devices;
+    }
+
+    get selectedType() {
+        return this._selectedType;
+    }
+
+    get selectedBrand() {
+        return this._selectedBrand;
     }
 }
