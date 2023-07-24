7794be10068e05e0d2d33254d9b0885de697621d
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-07-24 16:44:31 +0300

    FRONTEND - Страница администратора, модальные окна

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -7,10 +7,11 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 
 import { Context } from '../index';
-import { SHOP_ROUTE } from '../utils/consts';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 
 const NavBar = observer(() => {
     const { user } = useContext(Context);
+    const navigate = useNavigate();
     return (
         <Navbar bg="dark" data-bs-theme="dark">
             <Container>
@@ -18,8 +19,19 @@ const NavBar = observer(() => {
                 {
                     user.isAuth ?
                         <Nav className="ml-auto" style={{ color: 'white' }}>
-                            <Button variant="outline-light">Адним панель</Button>
-                            <Button variant="outline-light" className='ms-1'>Войти</Button>
+                            <Button 
+                                variant="outline-light" 
+                                onClick={ () => navigate(ADMIN_ROUTE) }
+                            >   
+                                Адним панель
+                            </Button>
+                            <Button 
+                                variant="outline-light" 
+                                className='ms-1'
+                                onClick={ () => navigate(LOGIN_ROUTE) }
+                            >   
+                                Выйти
+                            </Button>
                         </Nav>
                     :
                         <Nav className="ml-auto" style={{ color: 'white' }}>
